a8f52a8590861c773e382e52f323b8481e1f49da
sonyapanasiuk <151020578+sonyavrag@users.noreply.github.com>
2024-03-22 12:38:42 +0200

    wip, added reading from stdout

--- v2.asm
+++ v2.asm
@@ -15,4 +15,36 @@ main:
     ; do something with [oneChar]
     or ax,ax
     jnz read_next
+
+    test ax,ax
+    jns oi1
+    mov cx,ax
+    mov ah, 02h
+    mov dl,'-'
+    int 21h
+    mov ax,cx
+    neg ax
+    ; кіл-ть цифр в сх
+    oi1:
+    xor cx,cx
+    mov bx,10
+    oi2:
+    xor dx,dx
+    div bx
+    push dx
+    inc cx
+    test ax,ax
+    jnz oi2
+    mov ah, 02h
+    oi3:
+    pop dx
+    cmp dl,9
+    jbe oi4
+    add dl,7
+    oi4:
+    add dl,'0'
+    int 21h
+    ;зациклення на к-ть цифр
+    loop oi3
+    ret
 end main
