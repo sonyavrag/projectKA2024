e5f1509a1504c88d48855ab01fbef53dbd9b2ebc
sonyapanasiuk <151020578+sonyavrag@users.noreply.github.com>
2024-03-22 12:42:25 +0200

    wip, added bubble sort programme

--- v2.asm
+++ v2.asm
@@ -16,6 +16,26 @@ main:
     or ax,ax
     jnz read_next
 
+    bubbleSort:
+    array DW 3, 2, 6, 4, 1
+count DW 5
+    mov cx, word ptr count
+    dec cx  ; count-1
+outerLoop:
+    push cx
+    lea si, array
+innerLoop:
+    mov ax, [si]
+    cmp ax, [si+2]
+    jl nextStep
+    xchg [si+2], ax
+    mov [si], ax
+nextStep:
+    add si, 2
+    loop innerLoop
+    pop cx
+    loop outerLoop
+
     test ax,ax
     jns oi1
     mov cx,ax
